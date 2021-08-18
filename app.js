90487186515b03fa98d5bb4dc77ced48b8086d29
JoackoPadin <joacopadins@gmail.com>
2021-08-18 19:39:59 -0300

    Primer  commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const path = require ("path");
+const express = require ("express");
+const { dirname } = require("path");
+const app = express ()
+app.use(express.static(path.join(__dirname, "public")))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/home.html"))
+});
+app.listen (3000, () => console.log ("Servidor on Fire"))
+
+

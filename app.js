a84c5468f6f643fbe9152b55b388f427fd73bc68
JoackoPadin <joacopadins@gmail.com>
2021-08-18 20:30:05 -0300

    heroku

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const path = require ("path");
-const express = require ("express");
+const path = require("path");
+const express = require("express");
 const { dirname } = require("path");
-const app = express ()
-app.use(express.static(path.join(__dirname, "public")))
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "views/home.html"))
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
 });
-app.listen (3000, () => console.log ("Servidor on Fire"))
-
-
+app.listen(process.env.PORT || 3000, () => console.log("Servidor on Fire"));

ef6e57a54fdd83a1b1526220ebbd25c592ee8a34
JoackoPadin <joacopadins@gmail.com>
2021-08-23 20:52:28 -0300

    mayusculas

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const { dirname } = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/home.html"));
 });
